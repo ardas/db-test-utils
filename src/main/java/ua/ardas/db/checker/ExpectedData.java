649d80fe7a9e4633a4d4f1eb4d79387b1f13fa52
Alex Chernysh <alex@ardas.dp.ua>
2025-12-27 15:12:11 +0100

    chore: add `@Getter` annotation to `ExpectedData` and remove redundant getter method

--- src/main/java/ua/ardas/db/checker/ExpectedData.java
+++ src/main/java/ua/ardas/db/checker/ExpectedData.java
@@ -1,12 +1,14 @@
 package ua.ardas.db.checker;
 
 import com.google.common.collect.Lists;
+import lombok.Getter;
 
 import java.util.List;
 
 public class ExpectedData {
     public static final String NULL = "__NULL__";
 
+    @Getter
     private final List<List<String>> data = Lists.newArrayList();
 
     public ExpectedData addRow(String... values) {
@@ -14,7 +16,4 @@ public class ExpectedData {
         return this;
     }
 
-    public List<List<String>> getData() {
-        return data;
-    }
 }
